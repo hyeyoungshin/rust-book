2a0659a81c391d0ad1bb9037fd15cb16c249dd01
Hyeyoung Shin <hyeyoungshinw@gmail.com>
2025-11-30 15:23:38 -0700

    finished

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -2,20 +2,38 @@
 // Programmning a Guessing Game
 
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is: {secret_number}");
 
-    println!("Please input your guess.");
+    
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new(); // `let mut` allows programmer to reassign 
-                                   // the variable `guess`
+        let mut guess = String::new(); // `let mut` allows programmer to reassign the variable `guess`
 
-    io::stdin()
-        .read_line(&mut guess) // `&mut` giallows the function to modify what 
-                               // `guess` points to
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess) // `&mut` giallows the function to modify what `guess` points to
+            .expect("Failed to read line");
 
-    println!("You guessed: {guess}");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue // go to the next iteration
+        };
+        
+        println!("You guessed: {guess}");
 
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
