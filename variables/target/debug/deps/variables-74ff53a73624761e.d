3bca4500f17c655cc6edfc859111e7d8c6fe2ec1
Hyeyoung Shin <hyeyoungshinw@gmail.com>
2026-01-08 20:20:09 -0700

    Stop tracking target directory

--- variables/target/debug/deps/variables-74ff53a73624761e.d
+++
@@ -1,5 +0,0 @@
-/home/hyeyoung/git/rust-book/variables/target/debug/deps/variables-74ff53a73624761e: src/main.rs
-
-/home/hyeyoung/git/rust-book/variables/target/debug/deps/variables-74ff53a73624761e.d: src/main.rs
-
-src/main.rs:
--- variables/target/debug/variables.d
+++
@@ -1,1 +0,0 @@
-/home/hyeyoung/git/rust-book/variables/target/debug/variables: /home/hyeyoung/git/rust-book/variables/src/main.rs
